1e8545ead71c79cfd58416e8ea5451657683c953
Filipe-Souza-Machado <159461643+Filipe-Souza-Machado@users.noreply.github.com>
2024-08-04 21:40:14 -0300

    Add files via upload

---
+++ 1.conceitos Basicos/1.4-Operadores_aritimeticos.kt
@@ -0,0 +1,23 @@
+fun main() {
+    
+   //operadores aritimeticos:
+
+    var n1 = 24;
+    var n2 = 2;
+    
+    n1 ++;
+    println("Incrementação: $n1");
+
+    n1 --;
+    println("Decrementação: $n1");
+
+    n1 *= 2;
+    println("Multiplicação: $n1");
+
+    n1 /= 2;
+    println("Divisão: $n1");
+
+    n1 % n2 ;
+    println("modulo: $n1");
+    
+}
